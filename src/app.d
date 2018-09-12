ac196ebc4b45453542ce950cac402a363ca6e4d3
ARaspiK <araspik@protonmail.com>
2018-09-12 20:02:54 +0100

    Added basic build setting reading support.
    + Added a Command type representing single commands to execute
      + Basic time-stamp checking support exists to determine whether an
        update is needed
      + Updates are not necessary if one or more outputs are missing.
    + Added a Project type representing project metadata
      + Contains project name, languages (just strings), and version
      + version is partially conforming to Semantic Versioning.
    + Added a SemVer type representing Semantic Versioning (incomplete)
    + Added a basic printer that reads from a "stuff.sdl" file
    + Added a `docs` folder with an example build setup.
    
    Signed-off-by: ARaspiK <araspik@protonmail.com>

--- src/app.d
+++ src/app.d
@@ -1,3 +1,30 @@
 module app;
 
 import sdlang;
+
+import std.algorithm: map;
+import std.stdio;
+
+import project;
+import command;
+
+void main(string[] args) {
+
+  Tag root = "stuff.sdl".parseFile;
+
+  Project proj = Project(root.expectTag("project"));
+
+  writeln("Project: ", proj.name);
+  writeln("Languages: ", proj.langs);
+  writeln("Version: ", proj.ver);
+
+  foreach (c; root.tags["cmd"].map!(t => Command(t))) {
+    writeln("* Command \"", c.name, "\"");
+    writeln("  Sources: ", c.sources);
+    writeln("  Outputs: ", c.outputs);
+    writeln("  Commands: ", c.commands);
+    writeln("  Dependencies: ", c.depends);
+
+    writeln("  Needs updating: ", c.sourceUpdateTime > c.outputUpdateTime);
+  }
+}
